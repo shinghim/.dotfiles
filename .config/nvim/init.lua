7dddc41ebc44a3173ba3c9c781e1125d84cff8f8
Shing Him Ng <shinghim@protonmail.com>
2025-11-10 09:49:10 -0600

    Only load file tree on nvim without args

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,7 +38,10 @@ end)
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    vim.cmd("NvimTreeToggle")
+    -- only open NvimTree if no files were passed to Neovim (normal startup)
+    if vim.fn.argc() == 0 then
+      vim.cmd("NvimTreeToggle")
+    end
   end
 })
 
