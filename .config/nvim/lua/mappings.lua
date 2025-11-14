3808ef3558a49378b4c40d19098409f71d45bcbf
Shing Ng <shing.ng@pmg.com>
2025-11-14 14:03:42 -0600

    Unhighlight on <Esc> if no floating windows are open

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,11 +7,17 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- Close floating windows on <Esc>
+-- Close floating windows or clear highlights on <Esc> 
 map("n", "<Esc>", function()
+  local closed = false
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     if vim.api.nvim_win_get_config(win).relative ~= "" then
       vim.api.nvim_win_close(win, true)
+      closed = true
     end
   end
-end, { desc = "close floating window" })
+  if not closed then
+    vim.cmd("nohlsearch")
+  end
+end, { desc = "close floating window or clear highlights" })
+
