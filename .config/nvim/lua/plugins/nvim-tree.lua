89ae6634829016f72d3ec4ecfae298b8f59f94d7
Shing Ng <shing.ng@pmg.com>
2025-11-24 16:36:23 -0600

    Add on_attach to restore NvimTree default keymaps

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -3,6 +3,13 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup({
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set('n', 'h', api.node.navigate.parent_close, { buffer = bufnr, noremap = true, silent = true })
+        vim.keymap.set('n', 'l', api.node.open.edit, { buffer = bufnr, noremap = true, silent = true })
+      end,
+
       update_focused_file = {
         enable = true,
         update_root = false,
@@ -10,7 +17,7 @@ return {
       filesystem_watchers = { enable = true },
     })
 
-  vim.api.nvim_set_hl(0, "NvimTreeOpenedFile", { fg = "#88c0d0", bg = "#2a2a2a", bold = true })
-  vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#3a3a4a" })
+    vim.api.nvim_set_hl(0, "NvimTreeOpenedFile", { fg = "#88c0d0", bg = "#2a2a2a", bold = true })
+    vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#3a3a4a" })
   end,
 }
