8873bd1c0949996593dbed1ef4d88b89299f7597
Shing Ng <shing.ng@pmg.com>
2026-01-29 16:44:15 -0600

    Simplify tree-sitter-context

--- .config/nvim/lua/configs/treesitter.lua
+++
@@ -1,4 +0,0 @@
-return {
-  ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc", "rust", "go", "python", "terraform", "dockerfile" },
-  highlight = { enable = true},
-}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -59,11 +59,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate | TSInstallAll",
-    opts = function()
-      return require "configs.treesitter"
-    end,
   },
 
   {
