b666183cc0e418ebd5df2ea9a0e37177c0f4e64f
Shing Ng <shing.ng@pmg.com>
2026-02-05 12:50:14 -0600

    Update telescope mappigns

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -2,9 +2,9 @@ require("telescope").setup {}
 
 vim.keymap.set("n", "<leader>fw", require("telescope.builtin").live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fd", require("telescope.builtin").find_files, { desc = "Find files (root)" })
-vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references, { desc = "Find references (root)" })
 
 -- List mappings
+vim.keymap.set("n", "<leader>lr", require("telescope.builtin").lsp_references, { desc = "List references (root)" })
 vim.keymap.set("n", "<leader>lb", require("telescope.builtin").buffers, { desc = "List buffers" })
 vim.keymap.set("n", "<leader>lr", require("telescope.builtin").registers, { desc = "List registers" })
 vim.keymap.set("n", "<leader>lq", require("telescope.builtin").quickfix, { desc = "List quickfix items" })
@@ -12,7 +12,7 @@ vim.keymap.set("n", "<leader>ld", function()
   require("telescope.builtin").diagnostics({ bufnr = 0 })
 end, { desc = "List diagnostics (current file)" })
 vim.keymap.set("n", "<leader>lc", require("telescope.builtin").git_status, { desc = "List changed files" })
-vim.keymap.set("n", "<leader>gi", require("telescope.builtin").lsp_implementations, { desc = "Goto implementations" })
+vim.keymap.set("n", "<leader>li", require("telescope.builtin").lsp_implementations, { desc = "List implementations" })
 vim.keymap.set("n", "<leader>en", function()
     require("telescope.builtin").find_files {
       cwd = vim.fn.stdpath("config")
