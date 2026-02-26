3943f66709715f36546ac6b7e49e10a985b84728
Shing Ng <shing.ng@pmg.com>
2026-02-26 12:24:25 -0600

    Ignore terminal type buffers in bufferline

--- .config/nvim/lua/configs/bufferline.lua
+++ .config/nvim/lua/configs/bufferline.lua
@@ -3,16 +3,18 @@ return {
     offsets = {
       {
         filetype = "NvimTree",
-        text="File Explorer",
-        separator= true,
+        text = "File Explorer",
+        separator = true,
         text_align = "left",
         offset = 1,
       },
     },
+    custom_filter = function(buf_number)
+      return vim.bo[buf_number].buftype ~= "terminal"
+    end,
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
     diagnostics = "nvim_lsp",
   }
 }
-
