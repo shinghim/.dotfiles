3a07f88384004fd9d89fceab708ffde04e2fa81b
Shing Ng <shing.ng@pmg.com>
2026-03-27 09:55:26 -0500

    Add git reset chunk

--- .config/nvim/lua/configs/gitsigns.lua
+++ .config/nvim/lua/configs/gitsigns.lua
@@ -4,4 +4,4 @@ gs.setup({})
 
 vim.keymap.set("n", "[c", gs.prev_hunk, { desc = "Previous git change" })
 vim.keymap.set("n", "]c", gs.next_hunk, { desc = "Next git change" })
-
+vim.keymap.set("n", "<leader>Gr", gs.reset_hunk, { desc = "Reset git change" })
