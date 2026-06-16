928a06d9e1c73bcac13f636269329c3bd1403732
Shing Ng <shing.ng@pmg.com>
2026-06-16 17:02:44 -0500

    Add git diff keybinding

--- .config/nvim/lua/configs/gitsigns.lua
+++ .config/nvim/lua/configs/gitsigns.lua
@@ -1,7 +1,12 @@
 local gs = require("gitsigns")
 
-gs.setup({})
+gs.setup({
+  preview_config = {
+    border = "single",
+  }
+})
 
 vim.keymap.set("n", "[c", gs.prev_hunk, { desc = "Previous git change" })
 vim.keymap.set("n", "]c", gs.next_hunk, { desc = "Next git change" })
 vim.keymap.set("n", "<leader>Gr", gs.reset_hunk, { desc = "Reset git change" })
+vim.keymap.set("n", "<leader>Gd", gs.preview_hunk, { desc = "Git diff on hunk" })
